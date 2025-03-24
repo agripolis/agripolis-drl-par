1bc199ad4c2028b71608ed9cc6c12d38f1d4e9ae
Changxing Dong <dong@iamo.de>
2025-03-24 11:04:55 +0100

    Test with multiprocessing

--- mlu/main.py
+++ mlu/main.py
@@ -56,30 +56,32 @@ min_c_reward = 0
 class mManager(BaseManager):
     pass
 
-class AgentProxy(BaseProxy):
+class mManagerProxy(BaseProxy):
     pass
+
 
 mManager.register("mAgent", Agent)
 mManager.register("mNoise", GaussNoise)
+mManager.register("mPQueue", queue.PriorityQueue)
 
 res=queue.PriorityQueue(QSIZE)
 zmq_port_base = 12 
 #initzmq(zmq_port_base)
 
 # parallelizing, sharing priority queues, memory and agent
-def simuls(zmqbaseport, i, nsim, ag, nois): #,  shqueue, shmem, shagent):
+def simuls(zmqbaseport, i, nsim, ag, nois, resq): #,  shqueue, shmem, shagent):
   initzmq(zmqbaseport)
   for s in range(nsim):
     closed=False
+    resq.put((i,s))
 
     #tp=subprocess.Popen(["python",  "testmp.py", str(i), str(zmqbaseport)] , stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
     tp=subprocess.Popen([agpy, "--ZMQ_PORT_BASE=" + str(zmqbaseport), inputfiles ], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-    for i in range(9):
+    for x in range(runs):
       if not closed:
          recv_message()
          send_beta(0.5)
          rstr = recv_ec()
-         #print(rstr)
          rc = recv_closed()
          if rc>0:
              closed = True
@@ -169,119 +171,129 @@ def get_ep(e):
     d_sig=0.1
     d_simu=5
     sig=min_sig
-    for s in range(simus):
-        if (s+1) % d_simu == 0:
-            sig += d_sig
-            sig = min(sig, max_sig)
-            noise.set_sigma(sig)
 
-        closed=False
-        #subprocess.run(agripolis, iniputfiles])
-        agp=subprocess.Popen([agpy, "--ZMQ_PORT_BASE=" + str(zmq_port_base), inputfiles ], 
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE,
-                text=True)
-
-        ep=[]
-        eprew=0
-        for r in range(runs):
-            if not closed:
-                data=recv_message()
-                st = get_state(data)
-                
-                if e > 0:
-                   stn=agent.memory.norm_state(st)[0]
-                   beta = agent.get_action(stn)[0]
-                else:
-                   beta = agent.get_action(st)[0]
-                #beta = np.clip(beta, min_beta, max_beta)
-                delta = noise()
-                #print(delta)
-                beta += delta[0]
-                beta = np.clip(beta, min_beta, max_beta)
-                send_beta(beta)
-
-                rew = recv_ec()
-                #print("beta, rew: ", beta, rew)
-                
-                rc = recv_closed()
-                if rc>0:
-                    closed=True
-
-            else:
-                st=[-1]
-                beta=-1
-                rew= recv_ec()
-
-
-            ep.append((st, [beta],  [rew]))
-            eprew += rew
-
-        res.put((-eprew, ep))
-        agp.wait()
-        #agpout, agperr = agp.communicate()
-        #print(agpout)
-        #print(agperr)
-
-
-    #print(res.queue)
-    x=0
-    while x < topn  and not res.empty():
-       t=res.get()
-       ep= t[1]
-       c_rew = -t[0]
-       if len(agent.memory) < agent.memory_size or c_rew > min_c_reward:
-          agent.memory.rpush_episode(t) 
-          min_c_reward = agent.memory.get_min_reward()
-          #print(c_rew, min_c_reward)
-       if c_rew > best_c_reward:
-           best_c_reward = c_rew
-       x+=1 
-
-    #print("mem_size: ", len(agent.memory), best_c_reward)
-    res.queue.clear()
-
-    #print("len: ", len(agent.memory), "\tBS: ", batch_size)
-    if len(agent.memory) > batch_size:
-        agent.memory.norm()
-        for i in range(topn):
-            agent.update(batch_size)  #learning()
-            
-    if e % 1 == 0:
-        testreward = testing(e)
-        #print(f'--test--: {testreward}')
-        if testreward > best_reward:
-            best_reward = testreward 
-            outputModel(e)
-        else:
-            print(testreward,'\n')
-
-outputMemory = True
-LenMem = 10000
-curlen  = 0
-
-if __name__ == '__main__':
-   mp.set_start_method('spawn')
-   t1 = datetime.now()
-   with mManager() as manager:
+    nproc = 22 # mp.cpu_count()-2
+    with mManager() as manager:
       mpAgent = manager.mAgent(len_in, idx=idx-1)
       mpNoise = manager.mNoise(sigma=sigma_noise)
+
       pss=[]
-      nproc = 2 # mp.cpu_count()-2
+      qss = []
       for i in range(0,2*nproc,2):
-          print(i)
-          p=Process(target=simuls, args=(i,i,int(simus/nproc), mpAgent, mpNoise))
+          # print(i)
+          q = manager.mPQueue(int(simus/nproc))
+          p=Process(target=simuls, args=(i,i,int(simus/nproc), mpAgent, mpNoise,q))
           pss.append(p)
+          qss.append(q)
       
       for p in pss:
           p.start()
 
       for x in pss:
           x.join()
-      
-      print ((datetime.now()-t1).total_seconds())
 
-      input("hallo")
+      for y in qss:
+          t=y.get()
+          print(y.qsize(), t[0], t[1])
+
+     # input("hallo")
+#     for s in range(simus):
+#         if (s+1) % d_simu == 0:
+#             sig += d_sig
+#             sig = min(sig, max_sig)
+#             noise.set_sigma(sig)
+# 
+#         closed=False
+#         #subprocess.run(agripolis, iniputfiles])
+#         agp=subprocess.Popen([agpy, "--ZMQ_PORT_BASE=" + str(zmq_port_base), inputfiles ], 
+#                 stdout=subprocess.PIPE,
+#                 stderr=subprocess.PIPE,
+#                 text=True)
+# 
+#         ep=[]
+#         eprew=0
+#         for r in range(runs):
+#             if not closed:
+#                 data=recv_message()
+#                 st = get_state(data)
+#                 
+#                 if e > 0:
+#                    stn=agent.memory.norm_state(st)[0]
+#                    beta = agent.get_action(stn)[0]
+#                 else:
+#                    beta = agent.get_action(st)[0]
+#                 #beta = np.clip(beta, min_beta, max_beta)
+#                 delta = noise()
+#                 #print(delta)
+#                 beta += delta[0]
+#                 beta = np.clip(beta, min_beta, max_beta)
+#                 send_beta(beta)
+# 
+#                 rew = recv_ec()
+#                 #print("beta, rew: ", beta, rew)
+#                 
+#                 rc = recv_closed()
+#                 if rc>0:
+#                     closed=True
+# 
+#             else:
+#                 st=[-1]
+#                 beta=-1
+#                 rew= recv_ec()
+# 
+# 
+#             ep.append((st, [beta],  [rew]))
+#             eprew += rew
+# 
+#         res.put((-eprew, ep))
+#         agp.wait()
+#         #agpout, agperr = agp.communicate()
+#         #print(agpout)
+#         #print(agperr)
+# 
+# 
+    #print(res.queue)
+
+    #TODO
+    # merge res-i
+
+#     x=0
+#     while x < topn  and not res.empty():
+#        t=res.get()
+#        ep= t[1]
+#        c_rew = -t[0]
+#        if len(agent.memory) < agent.memory_size or c_rew > min_c_reward:
+#           agent.memory.rpush_episode(t) 
+#           min_c_reward = agent.memory.get_min_reward()
+#           #print(c_rew, min_c_reward)
+#        if c_rew > best_c_reward:
+#            best_c_reward = c_rew
+#        x+=1 
+# 
+#     #print("mem_size: ", len(agent.memory), best_c_reward)
+#     res.queue.clear()
+# 
+#     #print("len: ", len(agent.memory), "\tBS: ", batch_size)
+#     if len(agent.memory) > batch_size:
+#         agent.memory.norm()
+#         for i in range(topn):
+#             agent.update(batch_size)  #learning()
+#             
+#     if e % 1 == 0:
+#         testreward = testing(e)
+#         #print(f'--test--: {testreward}')
+#         if testreward > best_reward:
+#             best_reward = testreward 
+#             outputModel(e)
+#         else:
+#             print(testreward,'\n')
+# 
+outputMemory = True
+LenMem = 10000
+curlen  = 0
+
+if __name__ == '__main__':
+   mp.set_start_method('spawn')
 
    for e in range(epochs):
        #if (e+1) % d_epocs == 0:
